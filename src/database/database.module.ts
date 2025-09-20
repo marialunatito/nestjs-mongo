1bf0aa3eb26fa19d36b80b28af4119eed12ab95b
Maria Luna <mlunat20@gmail.com>
2025-09-20 18:33:05 -0500

    add conecction with ongo

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,11 @@
-import { Module, Global } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
 
 const API_KEY = '12345634';
 const API_KEY_PROD = 'PROD1212121SA';
+
+const uri = 'mongodb://root:root@localhost:27017';
+const client = new MongoClient(uri);
 
 @Global()
 @Module({
@@ -10,7 +14,17 @@ const API_KEY_PROD = 'PROD1212121SA';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'MONGO',
+      useFactory: async () => {
+        const uri = 'mongodb://root:root@localhost:27017';
+        const client = new MongoClient(uri);
+        await client.connect();
+        const database = client.db('online-store');
+        return database;
+      },
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'MONGO'],
 })
 export class DatabaseModule {}
