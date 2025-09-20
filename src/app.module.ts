30e0c84fc196a6fa6b5fa7cb95290a803e66bee3
Maria Luna <mlunat20@gmail.com>
2025-09-20 18:33:14 -0500

    add endpoint of test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
-import { Module, HttpModule, HttpService } from '@nestjs/common';
+import { HttpModule, HttpService, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ProductsModule } from './products/products.module';
+import config from './config';
 import { DatabaseModule } from './database/database.module';
 import { enviroments } from './enviroments';
-import config from './config';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
